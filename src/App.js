5a226ef4fe5530062cd295744932cb31df741306
junhee0916 <junhee916@naver.com>
2022-01-12 19:00:41 +0900

    react part2 shop

--- src/App.js
+++ src/App.js
@@ -1,25 +1,89 @@
-import logo from './logo.svg';
+/* eslint-disable */
+import React, {useState} from 'react';
+import {Navbar, Container, NavDropdown, Nav} from 'react-bootstrap'
 import './App.css';
-
+import shoesData from './data'
+import DetailView from './Detail'
+import {Link, Route, Switch} from 'react-router-dom'
+import axios from 'axios';
 function App() {
+  let [shoes, changeShoes] = useState(shoesData)
+  let [wait, changeWait] = useState(true)
+  let [amount, changeAmount] = useState([10, 11, 12])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <Container>
+        <Navbar.Brand href="#home">Shoes Shop</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav>
+            <Nav.Link as={Link} to="/">Home</Nav.Link>
+            <Nav.Link as={Link} to="/detail">Detail</Nav.Link>
+            <Nav.Link eventKey={2} href="#memes">
+              Dank memes
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    <Switch>
+      <Route exact path="/">
+          <div className='Jumbotron'>
+            <h1>20% 할인</h1>
+            <p>내용 확인</p>
+            <p>
+              <input type="button" value="button"></input>
+            </p>
+          </div>
+          <div className='container'>
+            <div className='row'>
+            {
+              shoes.map((rstShoes, i) => {
+                return(
+                  <Card shoes={shoes[i]} image={i} key={i}/>
+                )
+              })
+            }
+            </div>
+            <button className='btn btn-primary' onClick={() => {
+              {
+                wait === true?<WaitLoading/>:null
+              }
+              axios.get('https://codingapple1.github.io/shop/data2.json')
+              .then((result) => {
+                changeWait(false)
+                changeShoes([...shoes, ...result["data"]])
+              })
+              .catch((err) => {console.log(err)})
+            }}>더보기</button>
+          </div>
+      </Route>
+      <Route path="/detail/:id">
+        <DetailView shoes={shoes} amount={amount} changeAmount={changeAmount}/>
+      </Route>
+    </Switch>
+
     </div>
   );
+}
+
+function Card(props){
+  return(
+    <div className='col-md-4'>
+      <img src={'https://codingapple1.github.io/shop/shoes'+ (props.image + 1) +'.jpg'} width="100%"/>
+      <h4>{props.shoes["title"]}</h4>
+      <p>{props.shoes["content"]} & {props.shoes["price"]}</p>
+  </div>
+  )
+}
+
+function WaitLoading(){
+  return(
+    <div>
+      <h2>wait loading</h2>
+    </div>
+  )
 }
 
 export default App;
---
+++ src/Detail.js
@@ -0,0 +1,62 @@
+import React, {useEffect, useState} from "react"
+import {useHistory, useParams} from 'react-router-dom'
+function DetailView(props){
+    let [inputValue, changeInputValue] = useState('');
+    useEffect(() => {
+        let timmer = setTimeout(() => {
+                        changeModal(false)
+                    }, 2000);
+        return () => {clearTimeout(timmer)}
+    },[]);
+    let {id} = useParams();
+    let history = useHistory();
+    let searchShoes = props.shoes.find(function(rstShoes){
+        return rstShoes.id == id
+    })
+    let [modal, changeModal] = useState(true)
+    return(
+      <div className="container">
+          {inputValue}
+        <input onChange={(e) => {changeInputValue(e.target.value)}}/>
+        <div className="row">
+          <div className="col-md-6">
+            <img src={'https://codingapple1.github.io/shop/shoes'+(Number(id)+1)+'.jpg'} width="100%" />
+          </div>
+          <div className="col-md-6 mt-4">
+            <h4 className="pt-5">{searchShoes["title"]}</h4>
+            <p>{searchShoes["content"]}</p>
+            <p>{searchShoes["price"]}</p>
+            <Info amount={props.amount}></Info>
+            <button className="btn btn-danger" onClick={() => {
+                let newArray = [...props.amount]
+                console.log('data array 확인: ',newArray)
+                newArray[0] = Number(newArray[0])-1
+                console.log('첫번째 component 확인: ', newArray[0])
+                props.changeAmount(newArray)
+            }}>주문하기</button> 
+            <button className="btn btn-danger" onClick={() => {history.goBack();}}>뒤로가기</button> 
+          </div>
+        </div>
+        {
+            modal === true?<Modal/>:null
+        }
+      </div> 
+    )
+}
+function Info(props){
+    return(
+        <div>
+            <p>재고: {props.amount[0]}</p>  
+        </div>
+    )
+}
+
+function Modal(){
+    return(
+        <div>
+            Modal check
+        </div>
+    )
+  }
+
+  export default DetailView
